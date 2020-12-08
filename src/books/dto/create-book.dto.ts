f78fca5a040fc0d419baa640693ba23a181d263d
Faraz <faraz.shahsavan@gmail.com>
2020-12-08 21:40:00 +0330

    Add values to database

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -4,9 +4,9 @@ export default class CreateBookDto {
   @ApiProperty({description:'Enter the name of the book: ', minLength: 1, default: 'Book' ,maxLength:20})
   readonly name: string;
 
-  @ApiProperty({description:'Enter the user\'s ID: ', default: 0})
+  @ApiProperty({description:'Enter the user\'s ID: ', default: 1})
   readonly userID: number;
 
-  @ApiProperty({description:'Enter the ID of the genre: ', default: [0]})
+  @ApiProperty({description:'Enter the ID of the genre: ', default: [1]})
   readonly genreIDs: number[];
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,6 +4,6 @@ export default class CreateUserDto {
   @ApiProperty({description:'Enter the user\'s name: ', minLength: 1, default: 'User' ,maxLength:20})
   readonly name: string;
   
-  @ApiProperty({description:'Enter the ID of the genre: ', default: [0]})
+  @ApiProperty({description:'Enter the ID of the genre: ', default: [1]})
   readonly books: number[] ;
 }
